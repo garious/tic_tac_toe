e827c2e63f8ec11078e05fca4dca40424691049d
saralein <sara.m.trujillo@gmail.com>
2018-03-05 20:44:37 -0600

    Add IO struct with read/write generics

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 mod board;
+mod io;
 mod presenter;
 
 use board::*;
+use io::*;
 
 fn main() {
-    let mut board = Board::new(3);
-    print!("{}", presenter::view(&board));
-    board.update(2, CellState::Cross);
-    board.update(1, CellState::Nought);
-    print!("{}", presenter::view(&board));
+    let stdio = std::io::stdin();
+    let input = stdio.lock();
+    let output = std::io::stdout();
+    let board = Board::new(3);
+    let clear_sequence = format!("{}[2J\n", 27 as char);
+    let mut console = IO::new(input, output, &clear_sequence);
+    console.print(&presenter::view(&board));
 }
